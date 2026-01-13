271658791297a47c28ef49eea62e33669bef3d9e
unknown <haadinurhakim@gmail.com>
2026-01-13 12:34:52 +0700

    sistem kolaboratif

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    serverActions: {
+      // Menaikkan batas ukuran body untuk Server Actions menjadi 50MB
+      bodySizeLimit: "50mb",
+    },
+  },
 };
 
 export default nextConfig;
